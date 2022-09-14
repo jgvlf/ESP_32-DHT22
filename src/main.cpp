f1a9933ad492e1c6060e1a5bf5aa59bc2f7a4661
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-09-14 20:48:39 -0300

    📚Add Libray. Include the "PubSubClient" Module

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h> 
+#include <PubSubClient.h>
 #include <WiFi.h> //Inclusão Biblioteca ESP32
 #include <DHT.h> //Inclusão Biblioteca Sensor DHT22
 #include <DHT_U.h> //Inclusão Biblioteca Sensor DHT22
